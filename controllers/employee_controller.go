39c0e2f8475af38e4dd14817bf22b98a63c7d29c
useryesa9@gmail.com <useryesa9@gmail.com>
2025-04-21 15:22:04 +0700

    membuat tampilan edit dan fungsinya

--- controllers/employee_controller.go
+++ controllers/employee_controller.go
@@ -6,9 +6,30 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
+
+func MethodOverride(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			w.Write([]byte("gagal parse form"))
+			return
+		}
+		if r.Method == http.MethodPost {
+			if method := r.FormValue("_method"); method != "" {
+				r.Method = strings.ToUpper(method)
+			}
+		} else {
+			w.Write([]byte("hanya mendukung mehod POST"))
+			return
+		}
+		next(w, r)
+	}
+}
 
 func IndexEmployee(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -104,6 +125,106 @@ func StoreEmployee(db *gorm.DB) http.HandlerFunc {
 		if result := db.Create(&employee); result.Error != nil {
 			w.Write([]byte("gagal menyimpan data"))
 			return
+		}
+
+		http.Redirect(w, r, "/employee", http.StatusSeeOther)
+	}
+}
+
+func EditEmployee(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		//mengambil id
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		// mengambil data
+		var employee models.Employee
+		if result := db.Find(&employee, id); result.Error != nil {
+			log.Println("gagal mengambil data")
+		}
+
+		//parsing data
+		data := struct {
+			Employee models.Employee
+			Title    string
+		}{
+			Employee: employee,
+			Title:    "Employee List",
+		}
+
+		fp := filepath.Join("view", "employee", "edit.html")
+		t, err := template.ParseFiles(fp)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		err = t.Execute(w, data)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func UpdateEmployee(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			w.Write([]byte("gagal parse form"))
+			return
+		}
+		name := r.FormValue("name")
+		position := r.FormValue("position")
+		email := r.FormValue("email")
+		phone := r.FormValue("phone")
+
+		if name == "" {
+			w.Write([]byte("nama tidak bileh kosong"))
+			return
+		}
+		if position == "" {
+			w.Write([]byte("position tidak bileh kosong"))
+			return
+		}
+		if email == "" {
+			w.Write([]byte("email tidak bileh kosong"))
+			return
+		}
+		if phone == "" {
+			w.Write([]byte("phone tidak bileh kosong"))
+			return
+		}
+
+		//mengambil id
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		employeeUpdate := models.Employee{
+			Name:     name,
+			Position: position,
+			Email:    email,
+			Phone:    phone,
+		}
+
+		var employee models.Employee
+		// db.First(&employee, id)
+		// db.Model(&employee).Updates(employeeUpdate)
+
+		// if result := db.Create(&employee); result.Error != nil {
+		// 	w.Write([]byte("gagal menyimpan data"))
+		// 	return
+		// }
+
+		if result := db.First(&employee, id); result.Error != nil{
+			 	w.Write([]byte("gagal mengambil data"))
+		}
+
+		if result := db.Model(&employee).Updates(employeeUpdate); result.Error != nil{
+			w.Write([]byte("gagal memperbarui data"))
 		}
 
 		http.Redirect(w, r, "/employee", http.StatusSeeOther)
--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,8 @@ func SetupRoutes(db *gorm.DB) *mux.Router {
 	router.HandleFunc("/employee", controllers.IndexEmployee(db)).Methods("GET")
 	router.HandleFunc("/employee/create", controllers.CreateEmployee()).Methods("GET")
 	router.HandleFunc("/employee/create", controllers.StoreEmployee(db)).Methods("POST")
+	router.HandleFunc("/employee/edit/{id}", controllers.EditEmployee(db)).Methods("GET")
+	router.HandleFunc("/employee/edit/{id}", controllers.MethodOverride(controllers.UpdateEmployee(db))).Methods("PUT", "POST")
 
 	return router
 }
